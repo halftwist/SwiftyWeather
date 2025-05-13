335884de9fa21e183aa369b04d7b2062a6692da4
John Kearon <jkearon@hotmail.com>
2025-05-13 09:08:42 -0400

    Initial Commit

---
+++ SwiftyWeather/SwiftyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyWeatherApp.swift
+//  SwiftyWeather
+//
+//  Created by John Kearon on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
